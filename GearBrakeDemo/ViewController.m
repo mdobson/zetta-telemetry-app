cb0e859c6f8c9dd60c96174199632c554acd88c1
Matthew Dobson <mdobson4@gmail.com>
2015-08-12 16:05:39 -0400

    Initial Commit

---
+++ GearBrakeDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GearBrakeDemo
+//
+//  Created by Matthew Dobson on 8/12/15.
+//  Copyright (c) 2015 Matthew Dobson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ GearBrakeDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GearBrakeDemo
+//
+//  Created by Matthew Dobson on 8/12/15.
+//  Copyright (c) 2015 Matthew Dobson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
